0ac9d96136a12e45cfeae8620b1f4af1c0f1f7e2
NareshGuru77 <gurulingan.nareshkumar@gmail.com>
2018-06-01 06:11:44 +0200

    pruning now loads checkpoints

--- deeplab/prune_model.py
+++ deeplab/prune_model.py
@@ -25,12 +25,15 @@ flags.DEFINE_string('pruning_logs_dir', './checkpoints_logs/pruning/',
 flags.DEFINE_string('frozen_graph_path', './atWorkData/inference/mobileNet_binary/mobilenet.pb',
                     'Path of the frozen inference graph')
 
-def load_graph_def():
-    with tf.gfile.GFile(FLAGS.frozen_graph_path, "rb") as f:
-        graph_def = tf.GraphDef()
+flags.DEFINE_string('checkpoint_path', './checkpoints_logs/train_logs/mobileNet/similar_01/model.ckpt-30000',
+                    'Path of the checkpoint')
 
-    graph_def.ParseFromString(f.read())
-    return graph_def
+# def load_graph_def():
+#     with tf.gfile.GFile(FLAGS.frozen_graph_path, "rb") as f:
+#         graph_def = tf.GraphDef()
+#
+#     graph_def.ParseFromString(f.read())
+#     return graph_def
 
 def main(unused_argv):
     tf.logging.set_verbosity(tf.logging.INFO)
@@ -38,23 +41,23 @@ def main(unused_argv):
     g = tf.Graph()
     with g.as_default():
 
-        graph_def = load_graph_def()
-        tf.import_graph_def(
-            graph_def,
-            input_map=None,
-            return_elements=None,
-            name=None,
-            op_dict=None,
-            producer_op_list=None)
+        # graph_def = load_graph_def()
+        # tf.import_graph_def(
+        #     graph_def,
+        #     input_map=None,
+        #     return_elements=None,
+        #     name=None,
+        #     op_dict=None,
+        #     producer_op_list=None)
 
         # Create global step variable
-        global_step = tf.train.get_global_step()
+        global_step = tf.train.get_or_create_global_step()
 
         # Parse pruning hyperparameters
         pruning_hparams = pruning.get_pruning_hparams().parse(FLAGS.pruning_hparams)
         print (pruning_hparams)
         # Create a pruning object using the pruning specification
-        p = pruning.Pruning(pruning_hparams, global_step=global_step)
+        p = pruning.Pruning(spec=pruning_hparams, global_step=global_step)
 
         # Add conditional mask update op. Executing this op will update all
         # the masks in the graph if the current global step is in the range
@@ -64,7 +67,7 @@ def main(unused_argv):
         # Add summaries to keep track of the sparsity in different layers during training
         p.add_pruning_summaries()
 
-        saver = tf.train.Saver(slim.get_variables_to_restore())
+        saver = tf.train.Saver(slim.get_variables_to_restore(exclude=['model_pruning']))
         sv = tf.train.Supervisor(graph=g,
                                  logdir=FLAGS.pruning_logs_dir,
                                  init_op=tf.global_variables_initializer(),
@@ -74,9 +77,11 @@ def main(unused_argv):
                                  saver=saver)
 
         with sv.managed_session(start_standard_services=False) as sess:
-
+            sv.start_queue_runners(sess)
+            sv.saver.restore(sess, FLAGS.checkpoint_path)
             # Update the masks by running the mask_update_op
             sess.run(mask_update_op)
+
 
 
 if __name__ == '__main__':
