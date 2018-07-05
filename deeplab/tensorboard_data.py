423501385f408ad76aff4c5d605d96b9beb230bd
NareshGuru77 <gurulingan.nareshkumar@gmail.com>
2018-07-05 21:10:52 +0200

    updated to work with many events

--- deeplab/tensorboard_data.py
+++ deeplab/tensorboard_data.py
@@ -3,39 +3,38 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 from deeplab.datasets import segmentation_dataset
+from scipy.signal import savgol_filter
+import copy
+import os
 
 flags = tf.app.flags
 
 FLAGS = flags.FLAGS
 
-flags.DEFINE_string('dataset', 'atWork_binary',
-                    'Name of the segmentation dataset.')
+flags.DEFINE_multi_string('dataset', 'atWork_binary',
+                          'Name of the segmentation dataset.')
 
-flags.DEFINE_string('events_path', None,
-                    'Path to events file.')
+flags.DEFINE_multi_string('events_dir', None,
+                          'Directory of the events file.')
 
 flags.DEFINE_integer('final_step', 30000,
                      'Final step of logs in the event file.')
 
 
-def main(unused_argv):
-    tf.logging.set_verbosity(tf.logging.INFO)
+def get_results(events_path, class_names, cls_to_iou):
 
     step = []
-
-    label_def = segmentation_dataset.get_label_def(FLAGS.dataset)
-    class_names = np.array(label_def.values())
-    class_names = np.array(['class/' + cls for cls in class_names])
-    loss_dict = {key: [] for key in class_names}
+    miou = []
+    cls_to_iou = copy.deepcopy(cls_to_iou)
     confusion_rc_len = len(class_names) + 1
 
     confusion_string = None
     confusion_matrix = None
-    for event in tf.train.summary_iterator(FLAGS.events_path):
+    for event in tf.train.summary_iterator(events_path):
         step.append(event.step)
         for v in event.summary.value:
             if np.any(class_names == v.tag):
-                loss_dict[v.tag].append(v.simple_value)
+                cls_to_iou[v.tag].append(v.simple_value)
 
             if event.step == FLAGS.final_step and v.tag == 'confusion_matrix':
                 confusion_string = np.array(v.tensor.string_val)
@@ -46,17 +45,41 @@ def main(unused_argv):
                                    1:confusion_rc_len, :]
                 confusion_matrix = np.array(confusion_matrix, dtype=np.uint32)
 
-    print('confusion matrix with class names: ', confusion_string)
-    print('confusion matrix: ', confusion_matrix)
+            if v.tag == 'miou_1.0':
+                miou.append(v.simple_value)
 
     step = np.unique(step)
-    for key, value in loss_dict.items():
-        plt.plot(step, value, label=key)
+
+    return step, miou, cls_to_iou, confusion_matrix, confusion_string
+
+
+def main(unused_argv):
+    tf.logging.set_verbosity(tf.logging.INFO)
+
+    for variant, event_dir in zip(FLAGS.dataset, FLAGS.events_dir):
+
+        event_file = os.path.join(event_dir, os.listdir(
+            event_dir)[0])
+        label_def = segmentation_dataset.get_label_def(variant)
+        class_names = np.array(label_def.values())
+        class_names = np.array(['class/' + cls for cls in class_names])
+        cls_to_iou = {key: [] for key in class_names}
+
+        (step, miou, _,
+         _, _) = get_results(event_file, class_names, cls_to_iou)
+
+        if 'size' in variant:
+            if len(miou) > 1:
+                miou = savgol_filter(miou, 5, 2)
+                plt.plot(step, miou, label=variant)
+            else:
+                plt.scatter(FLAGS.final_step, miou, marker='+',
+                            linewidths=2, label='quantized model')
 
     plt.legend()
     plt.show()
 
 
 if __name__ == '__main__':
-    flags.mark_flag_as_required('events_path')
+    flags.mark_flag_as_required('events_dir')
     tf.app.run()
