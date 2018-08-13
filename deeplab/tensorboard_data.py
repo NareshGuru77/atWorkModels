97523cdd8a2a60ab3c5efb35298ae38c99fd0536
NareshGuru77 <gurulingan.nareshkumar@gmail.com>
2018-08-13 23:44:54 +0200

    updated code for generating result plots

--- deeplab/tensorboard_data.py
+++ deeplab/tensorboard_data.py
@@ -85,40 +85,35 @@ def plot_class_ious_infer(iou_vals, percent_vals, set_fonts, combine):
     plt.bar(x + 0.3, iou_vals, width=0.3, zorder=3, label='IOU')
     plt.bar(x, percent_vals, width=0.3,
             align='center', zorder=3, label='Percentage of pixels')
-    # plt.plot(x, iou_vals + 1)
-    # plt.plot(x, percent_vals + 1)
     plt.xticks([])
-    #plt.yticks([])
     plt.grid(zorder=0, axis='y')
 
     plt.tick_params(axis='both', which='major', labelsize=set_fonts)
-    plt.xlabel('Combine every 3 classes', fontsize=set_fonts)
-    #plt.ylabel('Sum of Normalized values', fontsize=set_fonts)
+    plt.xlabel('Combine every 2 classes', fontsize=set_fonts)
     plt.legend(bbox_to_anchor=(0., 1.02, 1., .102),
                loc=3, ncol=3,
                borderaxespad=0., prop={'size': set_fonts})
-    plt.tight_layout()
 
 
 def plot_class_ious(cls_to_iou, set_fonts,
                     cls_to_percentage):
 
-    cls_to_iou = {key.split('/')[-1]: np.mean(value) # value[-1]
+    cls_to_iou = {key.split('/')[-1]: np.mean(value)
                   for key, value in cls_to_iou.items()}
 
     cls_to_iou.pop('background', None)
     cls_to_percentage.pop('background', None)
 
-    max_class_iou = np.max(cls_to_iou.values())
-    cls_to_iou = {key: value/max_class_iou
-                 for key, value in cls_to_iou.items()}
+    sum_class_iou = np.sum(cls_to_iou.values())
+    cls_to_iou = {key: value/sum_class_iou
+                  for key, value in cls_to_iou.items()}
 
-    max_percentage = np.max(cls_to_percentage.values())
-    cls_to_percentage = {key: value/max_percentage
-                      for key, value in cls_to_percentage.items()}
+    sum_percentage = np.sum(cls_to_percentage.values())
+    cls_to_percentage = {key: value/sum_percentage
+                         for key, value in cls_to_percentage.items()}
 
     sorted_classes = np.array(sorted(cls_to_percentage.items(),
-                            key=operator.itemgetter(1)))[:,0]
+                              key=operator.itemgetter(1)))[:,0]
 
     iou_vals = [cls_to_iou[cls_iou] for
                 cls_iou in sorted_classes]
@@ -127,9 +122,9 @@ def plot_class_ious(cls_to_iou, set_fonts,
 
     x = np.arange(0, len(sorted_classes))
 
-    figure = plt.figure()
+    figure = plt.figure(figsize=(7, 6))
     figure.add_subplot(2, 1, 1)
-    plot_class_ious_infer(iou_vals, percentage_vals, set_fonts, 3)
+    plot_class_ious_infer(iou_vals, percentage_vals, set_fonts, 2)
 
     figure.add_subplot(2, 1, 2)
     plt.bar(x + 0.3, iou_vals, width=0.3, zorder=3, label='IOU')
@@ -140,9 +135,6 @@ def plot_class_ious(cls_to_iou, set_fonts,
 
     plt.tick_params(axis='both', which='major', labelsize=set_fonts)
     plt.ylabel('Normalized values', fontsize=set_fonts)
-    # plt.legend(bbox_to_anchor=(0., 1.02, 1., .102),
-    #            loc=3, ncol=3,
-    #            borderaxespad=0., prop={'size': set_fonts})
     plt.tight_layout()
     plt.show()
 
@@ -156,7 +148,7 @@ def plot_miou_bars(miou, set_fonts, labels_list):
 
     collect_mious = np.reshape(collect_mious, (8, 4))
 
-    mob_miou = collect_mious[0:4,:]
+    mob_miou = collect_mious[0:4, :]
     mob_miou = np.array(mob_miou)[np.newaxis].T
     xcep_miou = collect_mious[4:8, :]
     xcep_miou = np.array(xcep_miou)[np.newaxis].T
@@ -166,10 +158,10 @@ def plot_miou_bars(miou, set_fonts, labels_list):
 
     x_vals = np.arange(0, 15, 2)
     x_ticks = ['full\nMobileNetv2', 'full\nXception', 'size\nMobileNetv2',
-               'size\nXception', 'shape\nMobileNetv2', 'shape\nXception'
-               ,'binary\nMobileNetv2', 'binary\nXception']
+               'size\nXception', 'shape\nMobileNetv2', 'shape\nXception',
+               'binary\nMobileNetv2', 'binary\nXception']
 
-    figure = plt.figure(figsize=(8, 4))
+    _ = plt.figure(figsize=(8, 4))
     plt.ylim([30, 100])
 
     for index, m in enumerate(mob_miou):
@@ -188,7 +180,7 @@ def plot_miou_bars(miou, set_fonts, labels_list):
 
     plot_objs = np.array(plot_objs)[np.newaxis].T
 
-    plot_objs = plot_objs.reshape(4,8)
+    plot_objs = plot_objs.reshape(4, 8)
     new_plots = []
 
     for obj in plot_objs:
@@ -199,6 +191,40 @@ def plot_miou_bars(miou, set_fonts, labels_list):
     plt.tick_params(axis='both', which='major', labelsize=set_fonts)
     plt.ylabel('mIOU (%)', fontsize=set_fonts)
     plt.legend(new_plots, labels_list, bbox_to_anchor=(0., 1.02, 1., .102),
+               loc=3, ncol=2,
+               borderaxespad=0., prop={'size': set_fonts})
+    plt.tight_layout()
+    plt.show()
+
+
+def plot_class_balance(cls_iou, set_fonts):
+
+    cls_iou_act = {key.split('/')[-1]: value[-1]
+                   for key, value in cls_iou[1].items()}
+    cls_iou_bal = {key.split('/')[-1]: value[-1]
+                   for key, value in cls_iou[0].items()}
+
+    sorted_classes = np.array(sorted(cls_iou_act.items(),
+                              key=operator.itemgetter(1)))[:,0]
+
+    x = np.arange(0, len(sorted_classes))
+
+    iou_vals_act = [cls_iou_act[c_i] for
+                    c_i in sorted_classes]
+    iou_vals_bal = [cls_iou_bal[c_i] for
+                    c_i in sorted_classes]
+
+    _ = plt.figure(figsize=(8, 4))
+    plt.bar(x + 0.3, iou_vals_act, width=0.3, zorder=3,
+            label='Before class balancing')
+    plt.bar(x, iou_vals_bal, width=0.3,
+            align='center', zorder=3, label='After class balancing')
+    plt.xticks(x, sorted_classes, rotation=80)
+    plt.grid(zorder=0, axis='y')
+
+    plt.tick_params(axis='both', which='major', labelsize=set_fonts)
+    plt.ylabel('mIOU (%)', fontsize=set_fonts)
+    plt.legend(bbox_to_anchor=(0., 1.02, 1., .102),
                loc=3, ncol=2,
                borderaxespad=0., prop={'size': set_fonts})
     plt.tight_layout()
@@ -230,7 +256,7 @@ def plot_mious(miou_list, step_list, labels_list, set_fonts,
                fontsize=set_fonts, labelpad=12)
     plt.ylabel('mIOU (%)', fontsize=set_fonts)
     plt.legend(plot_objs, labels_list,
-               fontsize=set_fonts, loc=4)
+               fontsize=set_fonts, loc=0)
     plt.tight_layout()
     plt.grid(zorder=0, linestyle='dotted')
     plt.show()
@@ -252,20 +278,34 @@ def plot_confusion(confusion_matrix, label_def):
     plt.show()
 
 
-def plot_quantization_results(miou_list, labels_list, set_fonts):
+def plot_quantization_results(miou_list, labels_list, set_fonts,
+                              colormap):
 
-    model_size_list = [8.7, 2.8, 165.6, 44.7]
+    model_size_list = [8.7]*4 + [2.8]*4 + [165.6]*4 + [44.7]*4
     miou = []
-    mark = ['o', 'd', '^', 's']
+    mark = ['o']*4 + ['d']*4 + ['^']*4 + ['s']*4
+    colors = [colormap(0.2), colormap(0.5), colormap(0.7), colormap(0.9)]*4
+    plot_objs = []
 
     for mo in miou_list:
         miou.append(mo[-1])
+    print(np.round(miou, 2))
 
     fig, ax = plt.subplots()
     for index, (sz, mo) in enumerate(zip(model_size_list, miou)):
-        ax.scatter(sz, mo, label=labels_list[index],
-                    marker=mark[index], linewidths=4)
+        plot_objs.append(ax.scatter(sz, mo, color=colors[index],
+                         marker=mark[index], linewidths=2))
         plt.ylim([60, 102])
+
+    plot_objs = np.reshape(plot_objs, (4, 4))
+    plot_objs_t = plot_objs.T
+
+    plot_objs = np.vstack((plot_objs, plot_objs_t))
+
+    new_plots = []
+
+    for obj in plot_objs:
+        new_plots.append(tuple(obj))
 
     rect = patches.Rectangle((0, 90), 10, 10, fill=False,
                              edgecolor='g', linewidth=2)
@@ -275,8 +315,39 @@ def plot_quantization_results(miou_list, labels_list, set_fonts):
                     labelsize=set_fonts)
     plt.xlabel('Occupied disk memory',
                fontsize=set_fonts, labelpad=12)
-    plt.ylabel('Mean IOU (%)', fontsize=set_fonts)
-    plt.legend(fontsize=set_fonts, loc=4)
+    plt.ylabel('mIOU (%)', fontsize=set_fonts)
+    leg = plt.legend(new_plots, labels_list, fontsize=set_fonts, loc=4, ncol=2)
+    leg.legendHandles[2].set_color(colormap(0.1))
+    print(leg.legendHandles)
+    plt.tight_layout()
+    plt.grid(zorder=0, linestyle='dotted')
+    plt.show()
+
+
+def plot_quant(labels_list, set_fonts, colormap,):
+
+    model_size_list = [8.7] + [2.8] + [165.6] + [44.7]
+    infer_time = [0.9811, 1.4560, 5.5325, 7.6256]
+    mark = ['o'] + ['d'] + ['^'] + ['s']
+    colors = [colormap(0.2), colormap(0.5), colormap(0.7), colormap(0.9)]
+
+    fig, ax = plt.subplots()
+
+    rect = patches.Rectangle((0, 0), 10, 1, fill=False,
+                             edgecolor='g', linewidth=2, zorder=0)
+    ax.add_patch(rect)
+
+    for index, (siz, ti) in enumerate(zip(model_size_list, infer_time)):
+        ax.scatter(siz, ti, color=colors[index], label=labels_list[index],
+                   marker=mark[index], linewidths=2)
+
+    plt.tick_params(axis='both', which='major',
+                    labelsize=set_fonts)
+    plt.ylim([0, 9])
+    plt.xlabel('Occupied disk memory',
+               fontsize=set_fonts, labelpad=12)
+    plt.ylabel('Inference time (s)', fontsize=set_fonts)
+    plt.legend(fontsize=set_fonts, loc=4, ncol=2)
     plt.tight_layout()
     plt.grid(zorder=0, linestyle='dotted')
     plt.show()
@@ -299,6 +370,7 @@ def main(unused_argv):
 
     miou_list = []
     step_list = []
+    iou_list = []
 
     for index, (dataset, event_dir) in enumerate(
             zip(FLAGS.dataset, FLAGS.events_dir)):
@@ -314,6 +386,7 @@ def main(unused_argv):
          confusion_matrix, _) = get_results(event_file, class_names, cls_to_iou)
         miou_list.append(miou)
         step_list.append(step)
+        iou_list.append(cls_to_iou)
 
         if FLAGS.metric_to_plot == 'class_iou':
             plot_class_ious(cls_to_iou, set_fonts,
@@ -323,40 +396,48 @@ def main(unused_argv):
         elif FLAGS.metric_to_plot == 'confusion_matrix':
             plot_confusion(confusion_matrix, label_def)
 
+    if FLAGS.metric_to_plot == 'class_balance':
+        plot_class_balance(iou_list, set_fonts)
+
     if FLAGS.metric_to_plot == 'miou':
-        # plot_mious(miou_list, step_list, FLAGS.dataset, set_fonts,
-        #            model_variant='xception')
+        plot_mious(miou_list, step_list, FLAGS.dataset, set_fonts,
+                   model_variant='mobileNet')
 
         miou_list = np.array(miou_list)
         step_list = np.array(step_list)
-        labels_list = ['VB: MobileNetv2', 'WB: MobileNetv2', 'VB: Xception', 'WB: Xception']
+        # labels_list = ['VB: MobileNetv2', 'WB: MobileNetv2', 'VB: Xception', 'WB: Xception']
         # labels_list = ['Real training data', 'VB all training data', 'WB all training data',
         #                'VB artificial training data']
         # labels_list = ['Real training data', 'VB artificial Training data',
         #                'WB all training data']
         #labels_list = ['Real training data', 'Artificial Training data']
         # labels_list = ['PASCAL pretrained', 'Binary pretrained']
-        # labels_list = ['mobileNet: PASCAL VOC 2012', 'mobileNet: atWork_binary',
-        #                'xception: PASCAL VOC 2012', 'xception: atWork_binary']
+        labels_list = ['MobileNetv2: PASCAL VOC 2012', 'MobileNetv2: atWork_binary',
+                       'Xception: PASCAL VOC 2012', 'Xception: atWork_binary']
         # labels_list = ['VB: PASCAL pretrained', 'VB: Binary pretrained',
         #                'WB: PASCAL pretrained', 'WB: Binary pretrained']
         # labels_list = ['BW: actual', 'BW: 0.3', 'BW: 0.6', 'BW: 0.9']
         # labels_list = ['cosine restarts', 'poly']
 
         select_idx = [idx for idx, dataset in enumerate(FLAGS.dataset)
-                      if 'binary' in dataset]
-        plot_mious(miou_list[select_idx], step_list[select_idx],
-                   labels_list, set_fonts, model_variant='xception')
+                      if 'full' in dataset]
+        # plot_mious(miou_list[select_idx], step_list[select_idx],
+        #            labels_list, set_fonts, model_variant='xception')
 
         # plot_miou_bars(miou_list, set_fonts, labels_list)
 
     if FLAGS.metric_to_plot == 'quant_results':
         miou_list = np.array(miou_list)
-        select_idx = [idx for idx, dataset in enumerate(FLAGS.dataset)
-                      if 'shape' in dataset]
-        labels_list = ['mobileNet', 'mobileNet_8bit', 'xception', 'xception_8bit']
-        plot_quantization_results(miou_list[select_idx],
-                                  labels_list, set_fonts)
+        labels_list = ['MobileNetv2', 'MobileNetv2-8bit', 'Xception', 'Xception-8bit',
+                       'atWork_full', 'atWork_size_invariant', 'atWork_similar_shapes',
+                       'atWork_binary']
+        plot_quantization_results(miou_list,
+                                  labels_list, set_fonts, colormap)
+
+    if FLAGS.metric_to_plot == 'plot_quant':
+        labels_list = ['MobileNetv2', 'MobileNetv2-8bit', 'Xception', 'Xception-8bit']
+        plot_quant(labels_list, set_fonts, colormap)
+
 
 if __name__ == '__main__':
     flags.mark_flag_as_required('events_dir')
